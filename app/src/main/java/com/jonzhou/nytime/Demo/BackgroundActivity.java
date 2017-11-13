302dddf19f30f63ec23cc458a25ccd49f9cb494b
Jon <huaiyiheyuan@gmail.com>
2017-11-13 22:19:22 +0800

    button背景颜色

---
+++ app/src/main/java/com/jonzhou/nytime/Demo/BackgroundActivity.java
@@ -0,0 +1,29 @@
+package com.jonzhou.nytime.Demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.jonzhou.nytime.R;
+import com.stx.xhb.xbanner.XBanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 按钮背景颜色设置　背景变化
+ */
+public class BackgroundActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background);
+    }
+
+
+}
