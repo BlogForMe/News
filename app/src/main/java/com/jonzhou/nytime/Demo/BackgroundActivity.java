21efdcd4864015a833db2a90d10152cf1ebf9122
oschina <a18720629586@163.com>
2017-11-30 17:05:21 +0800

    viewstub

--- app/src/main/java/com/jonzhou/nytime/Demo/BackgroundActivity.java
+++ app/src/main/java/com/jonzhou/nytime/Demo/BackgroundActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.TextView;
 
 import com.jonzhou.nytime.R;
 
@@ -26,6 +27,9 @@ public class BackgroundActivity extends AppCompatActivity {
 
     public void btBgChange(View v) {
         View iv_vsContent = viewStub.inflate();
+        TextView tvText = (TextView) iv_vsContent.findViewById(R.id.tv_text);
+        tvText.setText("你好");
+
     }
 
 
