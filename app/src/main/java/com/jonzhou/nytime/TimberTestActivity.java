8fb7b59298a492c7e6581bb8af80feb53a7bd8ef
Jon <huaiyiheyuan@gmail.com>
2017-12-09 17:04:46 +0800

    Timner使用

--- app/src/main/java/com/jonzhou/nytime/TimberTestActivity.java
+++ app/src/main/java/com/jonzhou/nytime/TimberTestActivity.java
@@ -12,9 +12,9 @@ public class TimberTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timber_test);
+        Timber.tag("DemoActivity");
     }
 
     public void btLOG(View v) {
-        Timber.d("提示");
     }
 }
